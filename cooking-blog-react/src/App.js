5bcfd0f986486741880fd69f7f9de255a6e98bac
Emma M. Neil <emmaneil722@gmail.com>
2020-06-19 14:27:14 -0400

    experimented with suggest-recipes form

--- cooking-blog-react/src/App.js
+++ cooking-blog-react/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class FormElement extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <label className="form-element">
+        <p>{this.props.name}</p>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </label>
+    );
+  }
 }
 
-export default App;
+class SuggestRecipesForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    /*alert("A name was submitted: " + this.state.value);*/
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form className="form" onSubmit={this.handleSubmit}>
+        <FormElement name="Name:" />
+        <FormElement name="Email:" />
+        <FormElement name="Recipe:" />
+        <input className="submit" type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default SuggestRecipesForm;
