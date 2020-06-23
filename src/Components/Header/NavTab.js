c9f9d0337f40fc81e1e242c6b1dcee1a5ced78b3
Emma M. Neil <emmaneil722@gmail.com>
2020-06-23 11:42:06 -0400

    added fonts

--- src/Components/Header/NavTab.js
+++ src/Components/Header/NavTab.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import "./Header.css";
 
-let NavTab = (props) => {
+const NavTab = (props) => {
   return (
-    <div>
+    <div className="nav-tab-text">
       <p>{props.name}</p>
     </div>
   );
