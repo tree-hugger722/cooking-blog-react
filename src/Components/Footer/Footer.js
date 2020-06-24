5d20f05db4ddd1edb1c0c0b11941525042ea68fb
Emma M. Neil <emmaneil722@gmail.com>
2020-06-24 10:17:51 -0400

    adjusted footer text and position

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,9 +6,7 @@ const Footer = () => {
   return (
     <footer>
       <div>
-        <p>Contact us:</p>
-        <p>emmamyriamcooking@gmail.com</p>
-        <p>Copyright (c) 2020 Emma Neil</p>
+        <p id="footer-text">Copyright (c) 2020 Emma Neil</p>
       </div>
     </footer>
   );
